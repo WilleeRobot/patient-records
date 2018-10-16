3a054d1d49dbaec13e490e583e1e92b811d0c560
freeWillee <will.i.lee@outlook.com>
2018-10-15 22:24:03 -0600

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,10 @@
-#create users
+#create users, patient
+seb = User.create(username: "sebby", password: "sebby123")
+seb.save
+seb.patients.build(name: "Doggy", birth_year: "2010", phone: "201839281", address: "123 Main St", healthcarenumber: "828382112")
+seb.save
+
+h1 = History.create(date: "2017-08-01", notes: "Came in with a bald spot, complete physical", action: "Prescribed doggy rogaine", patient: Patient.first)
+
+h1.save
+
